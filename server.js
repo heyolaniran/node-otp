91e092958f48cd299cfca55f8197e6fb6076ff60
heyolaniran <olaniran.abd@gmail.com>
2023-10-27 10:13:41 +0000

    Revert "fix: env variables"
    This reverts commit 9179a537d8cd4827df8b1db765f2efee8d01a248.

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
-require("dotenv").config() ; 
 const express = require('express') ; 
 const app = express() ; 
 
-const sharedSecret = process.env.SHARED_SECRET ; 
-
-const port = process.env.PORT  ; 
+const port = process.env.PORT || 3000 ; 
 
 app.use(express.json()) ; 
 
